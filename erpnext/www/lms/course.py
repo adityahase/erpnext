f44276bffcaeb4768344648f2196d5a451e5f4cf
scmmishra <scm.mymail@gmail.com>
2018-10-08 17:18:11 +0530

    templates: add templates for LMS content
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- erpnext/www/lms/course.py
+++ erpnext/www/lms/course.py
@@ -1,31 +1,8 @@
 from __future__ import unicode_literals
-import erpnext.education.utils as utils
-from urlparse import urlparse, parse_qs
 import frappe
 
 
 def get_context(context):
-    if frappe.form_dict['course']:
-        # Save form_dict variables
-        program_name = frappe.form_dict["program"]
-        course_name = frappe.form_dict["course"]
-        content_name = frappe.form_dict["content"]
-        content_type = frappe.form_dict["type"]
-
-        # Get the required doctypes
-        current_course = frappe.get_doc("Course", course_name)
-        current_content = frappe.get_doc(content_type, content_name)
-
-        # Saving context variables for Jinja
-        context.content = current_content
-        context.course_name = course_name
-        context.program_name = program_name
-        context.content_type = content_type
-        context.next_content_type, context.next_content = get_next_content(content_name, content_type, current_course.get_content_info())
-
-def get_next_content(c_name, c_type, content_list):
-    try:
-        next = content_list[content_list.index([c_type, c_name]) + 1]
-        return next[0], next[1]
-    except IndexError:
-        return None, None
+    context.current_course = frappe.get_doc("Course", frappe.form_dict["course"])
+    context.current_content = frappe.get_doc("Content", frappe.form_dict["content"])
+    next_content = get_next_content()
