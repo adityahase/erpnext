4937583fedd89aa432796221acb64f20fb59d7e1
Aditya Hase <aditya@adityahase.com>
2019-02-02 00:19:35 +0530

    feat(tally): Fetch Company list from Tally server

--- erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
+++ erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.js
@@ -3,6 +3,46 @@
 
 frappe.ui.form.on('Tally Migration', {
 	refresh: function(frm) {
+		frm.dashboard.set_headline_alert('Please download and run \
+			<a target="_blank" href="https://github.com/adityahase/corsall/releases">CORS Proxy</a> \
+			before continuing');
 
-	}
+		frm.source_url = `http://${frm.doc.proxy_host}:${frm.doc.proxy_port}/http://${frm.doc.tally_host}:${frm.doc.tally_port}`;
+		frm.company_query = `<ENVELOPE>
+			<HEADER>
+				<VERSION>1</VERSION>
+				<TALLYREQUEST>Export</TALLYREQUEST>
+				<TYPE>Collection</TYPE>
+				<ID>Company</ID>
+			</HEADER>
+			<BODY>
+				<DESC>
+					<STATICVARIABLES>
+						<SVEXPORTFORMAT>$$SysName:XML</SVEXPORTFORMAT>
+					</STATICVARIABLES>
+				</DESC>
+			</BODY>
+		</ENVELOPE>`;
+	},
+	fetch_company_list: function(frm) {
+		$.ajax({
+			url: frm.source_url,
+			type: "POST",
+			contentType: "application/xml",
+			dataType: "text",
+			data: frm.company_query,
+			success: (result) => {
+				frm.call("get_tally_company_list",
+					{xml: result},
+					(r) => {
+						frm.set_df_property('company', 'options', r.message);
+						frm.refresh_field('company');
+						frm.set_df_property('company', 'reqd', 1);
+						frm.refresh_field('company');
+						frm.save();
+					}
+				);
+			}
+		});
+	},
 });
--- erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.py
+++ erpnext/erpnext_integrations/doctype/tally_migration/tally_migration.py
@@ -3,8 +3,36 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from bs4 import BeautifulSoup as bs
+
 import frappe
 from frappe.model.document import Document
+from frappe.utils import gzip_compress
+from frappe.utils.data import format_datetime
+from frappe.utils.file_manager import save_file
+
 
 class TallyMigration(Document):
-	pass
+	def autoname(self):
+		if not self.name:
+			self.name = "Tally Migration on "+ format_datetime(self.creation)
+
+	def get_tally_company_list(self, xml):
+		encoded_content = frappe.safe_encode(xml)
+		compressed_content = gzip_compress(encoded_content)
+		save_file(
+			fname="{}-{}-{}.xml.gz".format(self.doctype, self.name, "companies"),
+			content=compressed_content,
+			dt=self.doctype,
+			dn=self.name,
+			is_private=True,
+		)
+		companies = self.parse(xml, "COMPANY")
+		company_list = [company.NAME.string for company in companies]
+		return company_list
+
+	def parse(self, xml, entity):
+		response = bs(xml, "xml")
+		collection = response.ENVELOPE.BODY.DATA.COLLECTION
+		entities = collection.find_all(entity)
+		return entities
